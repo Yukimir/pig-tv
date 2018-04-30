265076dc86ac2e3732ea6da6c1a9fb03a1f2c686
yukimir <luodadi.huazhu@gmail.com>
2018-04-30 14:33:24 +0900

    fuck me

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const request = require('reqeust');
+const request = require('request');
 
 const liveStreams = [];
 const djStreams = [];
