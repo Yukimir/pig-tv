d2441509e7218e7c84f15e673e463296bd934f2e
yukimir <luodadi.huazhu@gmail.com>
2018-05-08 21:11:35 +0900

    random

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ class Stream {
 }
 const cq = new node_cqsocket_1.cqsocket('127.0.0.1', 9001);
 cq.listen(9002);
+cq.on('GroupMessage', (event) => {
+    if (event.ID === groupID) {
+        if (Math.random() < 0.05)
+            cq.SendGroupMessage(event.ID, event.message);
+    }
+});
 function emitMessage(message) {
     if (groupID === 0)
         return;
