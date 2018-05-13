dcd9315438fa10dfce84e5ef54c2a64714854faa
yukimir <luodadi.huazhu@gmail.com>
2018-05-13 17:10:18 +0900

    public

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
+    app.use(express.static(path.join(__dirname, 'public')))
     await app.listen(3000);
 }
 bootstrap();
