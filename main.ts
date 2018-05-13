571195dc8d5d9ce98549d55e6cd359738495a42e
yukimir <luodadi.huazhu@gmail.com>
2018-05-13 18:19:38 +0900

    f

--- main.ts
+++ main.ts
@@ -6,6 +6,11 @@ import * as path from 'path';
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
     app.use(express.static(path.join(__dirname, 'public')))
+    app.useGlobalFilters({
+        catch: (e: any, res: any) => {
+            console.log(e);
+        }
+    })
     await app.listen(3000);
 }
 bootstrap();
