be159b55530b4f21f5a169f286ff6b66aba7356b
yukimir <luodadi.huazhu@gmail.com>
2018-05-16 15:09:34 +0900

    yeah

--- src/app/core/qqbot.service.ts
+++ src/app/core/qqbot.service.ts
@@ -13,13 +13,11 @@ export class QQbotService {
         this.cq = new cqsocket(this.host, this.port);
         this.cq.listen(this.localPort);
         this.cq.on('GroupMessage', (event) => {
-            console.log(event);
             if (event.ID === this.groupID && Math.random() < 0.03) {
                 this.cq.SendGroupMessage(event.ID, event.message);
             }
         });
         this.cq.on('PrivateMessage', (event) => {
-            console.log('private', event);
             if (event.qq === this.myQQ) {
                 this.emitMessage(event.message);
             }
