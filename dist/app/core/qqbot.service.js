08901cadd9906e36c0fc2ab54e90aaf3df963202
yukimir <luodadi.huazhu@gmail.com>
2018-06-06 18:23:52 +0900

    change bot

--- dist/app/core/qqbot.service.js
+++ dist/app/core/qqbot.service.js
@@ -21,7 +21,7 @@ let QQbotService = class QQbotService {
         this.cq = new node_cqsocket_1.cqsocket(this.host, this.port);
         this.cq.listen(this.localPort);
         this.cq.on('GroupMessage', (event) => {
-            if (event.ID === this.groupID && Math.random() < 0.03) {
+            if (event.ID === this.groupID && Math.random() < 0.01) {
                 this.cq.SendGroupMessage(event.ID, event.message);
             }
         });
