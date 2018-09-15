0c7d5ca0fab5caa795ed94999887956f6e998120
yukimir <luodadi.huazhu@gmail.com>
2018-09-15 20:42:41 +0900

    disable qqbot

--- dist/app/streams/streams.service.js
+++ dist/app/streams/streams.service.js
@@ -49,7 +49,6 @@ let StreamsService = class StreamsService {
                     streamName: user.nickname
                 };
                 this.streams.push(stream);
-                this.qqbotService.emitMessage(`大佬${stream.streamName}走上了舞台，快来http://live.aigis.me:3000围观她~`);
                 this.wsGateWay.BoardCast('post-publish', {
                     id: stream.id,
                     app: stream.app,
